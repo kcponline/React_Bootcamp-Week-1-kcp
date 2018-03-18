96c07059462f44e1638638bd7bb81c5d51c50e26
Kent <kcponline@outlook.com>
2018-03-18 15:00:37 -0700

    add more static elements and remove margin=90% on row

--- src/Static/Static.jsx
+++ src/Static/Static.jsx
@@ -23,56 +23,77 @@ const Static = () => {
 			<div class="row">
 				<div class="col">
 					<h2>CHARGER 1</h2>
-		  			<p>OPEN</p>
-		  			<button class="btn-blue"><a href="#">UPDATE</a></button>
+		  			<p>STALL 1</p>
+		  			<button class="btn-blue"><a href="#">SELECT</a></button>
 				</div>
 				<div class="col">
 					<h2>CHARGER 2</h2>
 		  			<p>OPEN</p>
-		  			<button class="btn-blue"><a href="#">UPDATE</a></button>
+		  			<button class="btn-blue"><a href="#">SELECT</a></button>
 				</div>
 				<div class="col">
 					<h2>CHARGER 3</h2>
-		  			<p>OPEN</p>
-		  			<button class="btn-blue"><a href="#">UPDATE</a></button>
+		  			<p>RIGHT 2</p>
+		  			<button class="btn-blue"><a href="#">SELECT</a></button>
 				</div>
 			</div>
 
 			<div class="row">
 				<div class="col">
 					<h2>LEFT 2</h2>
-		  			<p>OPEN</p>
-		  			<button class="btn-blue"><a href="#">UPDATE</a></button>
+		  			<p>CHARGER:NONE</p>
+		  			<p>STALL:OPEN</p>
+		  			<p>START:N/A</p>
+		  			<p>EVANGEL:KENT</p>
+		  			<button class="btn-blue"><a href="#">SELECT</a></button>
 				</div>
 				<div class="col">
 					<h2>LEFT 1</h2>
-		  			<p>OPEN</p>
-		  			<button class="btn-blue"><a href="#">UPDATE</a></button>
+		  			<p>CHARGER:NONE</p>
+		  			<p>STALL:OPEN</p>
+		  			<p>START:N/A</p>
+		  			<p>EVANGEL:KENT</p>
+		  			<button class="btn-blue"><a href="#">SELECT</a></button>
 				</div>
 				<div class="col">
 					<h2>STALL 1</h2>
-		  			<p>OPEN</p>
-		  			<button class="btn-blue"><a href="#">UPDATE</a></button>
+		  			<p>CHARGER:1</p>
+		  			<p>CHARGING</p>
+		  			<p>START: 3:30PM</p>
+		  			<p>EVANGEL:KENT</p>
+		  			<button class="btn-blue"><a href="#">SELECT</a></button>
 				</div>
 				<div class="col">
 					<h2>STALL 2</h2>
-		  			<p>OPEN</p>
-		  			<button class="btn-blue"><a href="#">UPDATE</a></button>
+		  			<p>CHARGER:3</p>
+		  			<p>FINISHED</p>
+		  			<p>START: 2:30PM</p>
+		  			<p>EVANGEL:KENT</p>
+		  			<button class="btn-blue"><a href="#">SELECT</a></button>
 				</div>
 				<div class="col">
 					<h2>STALL 3</h2>
-		  			<p>OPEN</p>
-		  			<button class="btn-blue"><a href="#">UPDATE</a></button>
+		  			<p>CHARGER:NONE</p>
+		  			<p>WAITING</p>
+		  			<p>START: 5:20PM</p>
+		  			<p>EVANGEL:KENT</p>
+		  			<button class="btn-blue"><a href="#">SELECT</a></button>
 				</div>
 				<div class="col">
 					<h2>RIGHT 1</h2>
-		  			<p>OPEN</p>
-		  			<button class="btn-blue"><a href="#">UPDATE</a></button>
+		  			<p>CHARGER:NONE</p>
+		  			<p>WAITING</p>
+		  			<p>START: 4:45PM</p>
+		  			<p>EVANGEL:KENT</p>
+		  			<button class="btn-blue"><a href="#">SELECT</a></button>
 				</div>
 				<div class="col">
 					<h2>RIGHT 2</h2>
-		  			<p>OPEN</p>
-		  			<button class="btn-blue"><a href="#">UPDATE</a></button>
+		  			<p>CHARGER:3</p>
+		  			<p>CHARGING</p>
+		  			<p>START: 4:25PM</p>
+		  			<p>EVANGEL:KENT</p>
+		  			<button class="btn-blue"><a href="#">SELECT</a></button>
 				</div>
 				
 			</div>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/base.css';
-import './css/flexbox.css';
+import './css/base.css'; //add accordingly
+import './css/flexbox.css'; //add accordingly
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
