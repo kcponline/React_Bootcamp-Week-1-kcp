3d12fe2a3b91014e5ecdab1cb82172edd248a722
Kent <kcponline@outlook.com>
2018-03-17 23:38:24 -0700

    add Static\Static.jsx and inject html content and import/insert to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-
+import StaticComponent from './Static/Static'
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+      	<StaticComponent />
 
 
 
---
+++ src/Static/Static.jsx
@@ -0,0 +1,84 @@
+// import React, { Component } from 'react';
+import React from 'react';
+
+const Static = () => {
+	return (
+		<div>
+			<header class="main-header">
+				<h1 class="name"><a href="#">EV Helper</a></h1>
+		  		<ul class="main-nav">
+		        <li><a href="#">History</a></li>
+		        <li><a href="#">Account</a></li>
+		  		</ul>
+			</header>
+
+			<div class="banner">
+				<h1 class="headline">EV Helper</h1>
+		  		<span class="tagline">Helping you to get your EV Charging</span>
+			</div>
+
+			<div class="row">
+				<div class="col">
+					<h2>CHARGER 1</h2>
+		  			<p>OPEN</p>
+		  			<button class="btn-blue"><a href="#">UPDATE</a></button>
+				</div>
+				<div class="col">
+					<h2>CHARGER 2</h2>
+		  			<p>OPEN</p>
+		  			<button class="btn-blue"><a href="#">UPDATE</a></button>
+				</div>
+				<div class="col">
+					<h2>CHARGER 3</h2>
+		  			<p>OPEN</p>
+		  			<button class="btn-blue"><a href="#">UPDATE</a></button>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col">
+					<h2>LEFT 2</h2>
+		  			<p>OPEN</p>
+		  			<button class="btn-blue"><a href="#">UPDATE</a></button>
+				</div>
+				<div class="col">
+					<h2>LEFT 1</h2>
+		  			<p>OPEN</p>
+		  			<button class="btn-blue"><a href="#">UPDATE</a></button>
+				</div>
+				<div class="col">
+					<h2>STALL 1</h2>
+		  			<p>OPEN</p>
+		  			<button class="btn-blue"><a href="#">UPDATE</a></button>
+				</div>
+				<div class="col">
+					<h2>STALL 2</h2>
+		  			<p>OPEN</p>
+		  			<button class="btn-blue"><a href="#">UPDATE</a></button>
+				</div>
+				<div class="col">
+					<h2>STALL 3</h2>
+		  			<p>OPEN</p>
+		  			<button class="btn-blue"><a href="#">UPDATE</a></button>
+				</div>
+				<div class="col">
+					<h2>RIGHT 1</h2>
+		  			<p>OPEN</p>
+		  			<button class="btn-blue"><a href="#">UPDATE</a></button>
+				</div>
+				<div class="col">
+					<h2>RIGHT 2</h2>
+		  			<p>OPEN</p>
+		  			<button class="btn-blue"><a href="#">UPDATE</a></button>
+				</div>
+				
+			</div>
+
+			<footer class="main-footer">
+				<span>&copy;2018 React-CBC EV Helper</span>
+			</footer>
+		</div>
+	);
+}
+
+export default Static;
