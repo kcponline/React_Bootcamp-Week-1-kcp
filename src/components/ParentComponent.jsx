fee9ee436b7ddc3dca4e8cb28029dbc56380d17b
Kent <kcponline@outlook.com>
2018-04-20 21:56:17 -0700

    demo of comment out state in parentComponent

--- src/components/ParentComponent.jsx
+++ src/components/ParentComponent.jsx
@@ -6,29 +6,29 @@ import InfoHeader from './InfoHeader/InfoHeader';
 import CardList from './CardList/CardList';
 
 class ParentComponent extends Component {
-	state = {
-		chargername: "2",
-		stallname: "L1 L2",
-		openchargercount: 3,
-		openstallcount: 7,
-		chargers: [
-			{
-			station: "1",
-			chargerStatus: "STALL 1",
-			charging: true
-			},
-			{
-			station: "2",
-			chargerStatus: "OPEN",
-			charging: false
-			},
-			{
-			station: "3",
-			chargerStatus: "RIGHT 2",
-			charging: true
-			},
-		]
-	}
+	// state = {
+	// 	chargername: "2",
+	// 	stallname: "L1 L2",
+	// 	openchargercount: 3,
+	// 	openstallcount: 7,
+	// 	chargers: [
+	// 		{
+	// 		station: "1",
+	// 		chargerStatus: "STALL 1",
+	// 		charging: true
+	// 		},
+	// 		{
+	// 		station: "2",
+	// 		chargerStatus: "OPEN",
+	// 		charging: false
+	// 		},
+	// 		{
+	// 		station: "3",
+	// 		chargerStatus: "RIGHT 2",
+	// 		charging: true
+	// 		},
+	// 	]
+	// }
 
 	updateChargerName = (chargername) => {
 	// console.log('open charger name:', chargername);
@@ -45,7 +45,9 @@ class ParentComponent extends Component {
 	}
 
 	render() {
-		const { chargername, stallname, openchargercount, openstallcount, chargers } = this.state;
+		console.log ('parent component props', this.props);
+		// below is destructuring
+		const { chargername, stallname, openchargercount, openstallcount, chargers } = this.props;
 		return (
 			<div>
 				<InfoHeader
@@ -71,7 +73,13 @@ class ParentComponent extends Component {
 
 function mapStateToProps(state) {
 	console.log ('map state to props', state);
-	return state;
+	return {
+		chargers: state.chargers,
+		chargername: state.toolbar.chargername,
+		stallname: state.toolbar.stallname,
+		openchargercount: state.toolbar.openchargercount,
+		openstallcount: state.toolbar.openstallcount
+	};
 }
 
 export default connect(mapStateToProps)(ParentComponent);
