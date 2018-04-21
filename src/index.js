15f5be5b73e502cf90e1a8b5c2b82e0695df8508
Kent <kcponline@outlook.com>
2018-04-20 19:46:19 -0700

    add Provider component from react-redux

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css'; // add to use semantic-ui-css
 // import './css/base.css'; // add accordingly but remove if using semantic
 // import './css/flexbox.css'; // add accordingly but remove if using semantic
+import { Provider } from 'react-redux';
+import store from './store';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App /> 
+	</Provider>,
+	document.getElementById('root')
+);
 registerServiceWorker();
 
 // with semantic-ui, styling doesn't need to be defined
